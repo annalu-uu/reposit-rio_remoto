04c8f483fab73354fab391dab2b4af5c7050848c
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 16:35:39 -0300

    Criado os comandos fundamentais do js

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,92 @@ NESSE TIPO DE COMENTÁRIO
 */
 
 //CRIANDO VARIÁVEIS
+let num = 50
+var num2 = 180
+const num3 = 450
+var num5 = 540
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 420
+
+    console.log(num4)
+
+    console.log(num)
+}
+
+console.log(num5)
+
+num = 5
+
+console.log(num)
+
+num2 = 6
+
+console.log(num2)
+
+//num3 = 12
+
+//CONCATENAÇÃO
+console.log("Valor de num" + num)
+console.log("Valor de num3" , num3)
+console.log("Valor de num2 ${num2}")
+console.log('A soma dos números' + 15 + 8)
+console.log('A soma dos números' , 15 + 8)
+console.log('A soma dos números ${15 + 8}')
+console.log("A soma dos números" + num + num2)
+console.log("A soma dos números" , num2 + 8)
+console.log("A soma dos números ${num3 - num2")
+
+//OPERADORES MATEMÁTICOS
+/*
+  
+    + SOMA
+    - SUBTRAÇÃO
+    * MULTIPLICAÇÃO
+    / DIVISÃO
+    % MÓDULO (RESTO DA DIVISÃO)
+*/
+
+console.log('Soma das variáveis' , num + num2)
+console.log('subtração das variáveis' , num3 - num)
+console.log('Multiplicação' , num5 * 2)
+console.log('divisão de valores' , num2 / 5)
+console.log('Resto da divisão' , num2 % 2)
+console.log('Resto da divisão ${num3 % 3}')
+
+//OPERADORES RELACIONAIS
+/*
+
+    == IGUAL
+    > MAIOR
+    < MENOR
+    >= MAIOR IGUAL
+    <= MENOR IGUAL
+    != DIFERENTE
+
+*/
+
+//OPERADORES LÓGICOS
+/*
+ 
+    && E
+    || OU
+
+*/
+console.log('teste relacionais' , 10 == 5)
+console.log('teste relacionais' , 10 != 5)
+console.log('teste relacionais' , 10 > 5)
+console.log('teste relacionais' , 10 < 5)
+console.log('teste relacionais' , 10 >= 5)
+console.log('teste relacionais' , 10 <= 5) 
+console.log('teste lógico' , (10 > 5) && (15 < 8))
+console.log('teste lógico' , (10 > 5) || (15 < 8))
+
+
+    
+
+  
