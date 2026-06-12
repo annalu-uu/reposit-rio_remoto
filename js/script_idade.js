2ca775f82068af723a1149a6676184c1d4dfa50f
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-12 14:53:51 -0300

    criado new file

---
+++ js/script_idade.js
@@ -0,0 +1,10 @@
+ //PEGANDO ELEMENTOS DO DOM
+ const formDados - document.querySelector('formulario-pessoa')
+ const divResultado - document.querySelector('#div-dados')
+
+ //CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+ formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+ })
+ 
