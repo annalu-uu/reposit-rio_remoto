10f97563be8353bded1c2f6fb01d41d45da01582
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-12 16:26:05 -0300

    modificado o js

---
+++ js/scripts_questao01.js
@@ -0,0 +1,25 @@
+ //PEGANDO ELEMENTOS DO DOM
+ const formDados = document.querySelector('#formulario-pessoa')
+ const divResultado = document.querySelector('#div-dados')
+
+ //CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+ formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const objFormDados = new FormData(formDados)
+
+    let nome = objFormDados.get('nome')
+    let idade = objFormDados.get('idade')
+    
+    let situacaoIdade = ''
+
+    if (idade >= 18){
+        situacaoIdade = `${nome}, você é maior de idade`
+    }else{ 
+        situacaoIdade = `${nome}, você é menor de idade`
+    }
+
+    divResultado.innerHTML = situacaoIdade
+
+
+ })
