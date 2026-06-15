f73b80aaa652a03c70ed415c09c80c49b0ce8721
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-15 14:35:43 -0300

    adicionada nova pasta

---
+++ js/scripts_questao03.js
@@ -0,0 +1,24 @@
+ //PEGANDO ELEMENTOS DO DOM
+ const formDados = document.querySelector('#formulario')
+ const divResultado = document.querySelector('#div-dados')
+
+ //CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+ formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const objFormDados = new FormData(formDados)
+
+    let distância = parseFloat(objFormDados.get('distância'))
+    let consumo = parseFloat(objFormDados.get('consumo'))
+    let preço = parseFloat(objFormDados.get('preço'))
+
+    let quantidade = distância / consumo
+   
+
+
+    divResultado.innerHTML = `A quantidade de combustível necessário: ${quantidade.toFixed(2).replace('.',',')}L, 
+deslocamento: ${parseFloat(quantidade * preço).toFixed(2).replace('.',',')}R$`
+
+    formDados.request()
+
+ })
